417a1ee5d5f3c4265259c182154de1855b19f0c2
Namindu <namindu@live.com>
2023-11-20 22:13:52 +0530

    Namindu

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Convert pixel measurements to rem values
 ## Live version
 
 http://namindu.tk/works/px2rem/
+
+<img src="screenshot.png" width="200">
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,7 @@
 import Component from "./Component";
 import Display from "./Display";
 import Keypad from "./Keypad";
+const github_ico = require("./svg/github.svg");
 
 class Calculator extends Component{
     constructor (){
@@ -16,6 +17,10 @@ class Calculator extends Component{
         div.setAttribute('class', 'calculator');
         div.appendChild(this.display.render());
         div.appendChild(this.keypad.render());
+
+        var div2 = document.createElement('div');
+        div2.innerHTML = `<a href="https://github.com/naminduranathunga/px2rem/" target="_blank" style="margin-top:0.25rem; display:block;"><span class="small-icon">${github_ico}</span> <i>Github.com/naminduranathunga/px2rem/</i></a>`;
+        div.appendChild(div2);
         this.is_rendered = true;
         return div;
     }
